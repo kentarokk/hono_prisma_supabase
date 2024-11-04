942d2c65abb4eee371d3a6f2de886cf1c42daecd
kentarokk <ken.k.jul2@gmail.com>
2024-11-04 11:35:34 +0900

    pending: 環境変数が取得できていない

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+export const getPrisma = () => {
+  const prisma = new PrismaClient().$extends(withAccelerate());
+  return prisma;
+};
 
-export default app
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+  // return c.json({ message: c.env.DATABASE_URL as string });
+});
+
+app.get("/users", async (c) => {
+  const prisma = getPrisma();
+  const users = await prisma.user.findMany();
+  await prisma.$disconnect();
+  return c.json(users);
+});
+
+export default app;
